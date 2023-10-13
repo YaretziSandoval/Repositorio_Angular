23e33093894304378ed7cf63f5634eec435c23e4
Yaretzi Sandoval <12780@utl.edu.mx>
2023-10-13 09:25:03 -0600

    Actualizacion

--- angularYare/src/app/app.module.ts
+++ angularYare/src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { RouterModule, Routes } from '@angular/router';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { InicioComponent } from './inicio/inicio.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
+import { LoginComponent } from './login/login.component';
 
+
+const appRoutes: Routes=[
+  {path:'inicio', component:InicioComponent},
+  {path:'nosotros', component:NosotrosComponent},
+  {path:'login', component:LoginComponent}
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    NosotrosComponent,
+    LoginComponent
   ],
   imports: [
+    RouterModule.forRoot(
+    appRoutes, 
+    {enableTracing:true}
+  ),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
