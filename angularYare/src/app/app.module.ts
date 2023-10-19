c61021ad5d516948c4e2cde30ccdd2d8cace55c2
Yaretzi Sandoval <12780@utl.edu.mx>
2023-10-19 09:06:00 -0600

    Actualiza

--- angularYare/src/app/app.module.ts
+++ angularYare/src/app/app.module.ts
@@ -9,19 +9,22 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { InicioComponent } from './inicio/inicio.component';
 import { NosotrosComponent } from './nosotros/nosotros.component';
 import { LoginComponent } from './login/login.component';
+import { DatosComponent } from './datos/datos.component';
 
 
 const appRoutes: Routes=[
   {path:'inicio', component:InicioComponent},
   {path:'nosotros', component:NosotrosComponent},
-  {path:'login', component:LoginComponent}
+  {path:'login', component:LoginComponent},
+  {path:'datos', component:DatosComponent}
 ]
 @NgModule({
   declarations: [
     AppComponent,
     InicioComponent,
     NosotrosComponent,
-    LoginComponent
+    LoginComponent,
+    DatosComponent
   ],
   imports: [
     RouterModule.forRoot(
